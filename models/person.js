481e034ecc59e777d5a7f762d0d483540314aedc
aaalri <riku@RikWhite-PC.localdomain>
2021-09-16 21:40:28 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -14,8 +15,9 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name:{
         type: String,
-        minlength: 5,
-        required: true
+        minlength: 3,
+        required: true,
+        unique: true
     },
     number: {
         type: String,
@@ -23,6 +25,7 @@ const personSchema = new mongoose.Schema({
         required: true
     }
 })
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
