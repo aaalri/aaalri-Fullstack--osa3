8c1818f207c4c929d6930724807e150bc9a549a2
aaalri <riku@RikWhite-PC.localdomain>
2021-09-17 00:28:33 +0300

    uibuild

--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-  })
+  name: String,
+  number: String
+})
 
 if (process.argv.length<3) {
   console.log('missing password')
@@ -16,38 +16,38 @@ const url =
   `mongodb+srv://talkki:${password}@cluster0.gwldr.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`
 
 if (process.argv.length<4) {
-    
-    mongoose.connect(url)
 
-    const Person = mongoose.model('Person', personSchema)
+  mongoose.connect(url)
 
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-          console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-        process.exit(1)
+  const Person = mongoose.model('Person', personSchema)
+
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+    process.exit(1)
+  })
 
 } else {
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
 
-    mongoose.connect(url)
+  mongoose.connect(url)
 
-    const Person = mongoose.model('Person', personSchema)
+  const Person = mongoose.model('Person', personSchema)
 
-    const person = new Person({
-        name: name,
-        number: number
-    })
+  const person = new Person({
+    name: name,
+    number: number
+  })
 
-    person.save().then(response => {
-        console.log(`added ${name} number ${number} to phonebook`)  
-        mongoose.connection.close()
-        process.exit(1)
-    })
-    
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+    process.exit(1)
+  })
+
 }
